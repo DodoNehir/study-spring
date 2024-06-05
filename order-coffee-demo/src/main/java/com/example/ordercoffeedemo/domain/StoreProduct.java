fc30e92c8b76374204674cf81895a8255b0d5c26
DodoNehir <rojalaurant@gmail.com>
2024-06-05 10:29:31 +0900

    [feat/closed #1] create domain

--- order-coffee-demo/src/main/java/com/example/ordercoffeedemo/domain/StoreProduct.java
+++ order-coffee-demo/src/main/java/com/example/ordercoffeedemo/domain/StoreProduct.java
@@ -1,4 +1,22 @@
 package com.example.ordercoffeedemo.domain;
 
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Getter
+@Table(name = "store_products")
 public class StoreProduct {
+    @Id
+    private int storeProductId;
+
+    @Column
+    private String storeId;
+
+    @Column
+    private String productId;
+
+    @Column
+    private int stockQuantity;
 }
