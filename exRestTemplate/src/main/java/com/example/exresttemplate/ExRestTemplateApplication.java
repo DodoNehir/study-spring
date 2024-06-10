fbf9f857ae18afd6d4cb56bae223718844361673
DodoNehir <rojalaurant@gmail.com>
2024-06-10 22:49:15 +0900

    initial commit

---
+++ exRestTemplate/src/main/java/com/example/exresttemplate/ExRestTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.exresttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExRestTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExRestTemplateApplication.class, args);
+    }
+
+}
---
+++ exRestTemplate/src/test/java/com/example/exresttemplate/ExRestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exresttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExRestTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
