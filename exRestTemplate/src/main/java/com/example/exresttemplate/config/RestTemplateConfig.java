cdc14e611dc0f14b46689b3e3e26b577e19ee615
DodoNehir <rojalaurant@gmail.com>
2024-06-11 02:44:05 +0900

    RestTemplateConfig에 설명 추가

--- exRestTemplate/src/main/java/com/example/exresttemplate/config/RestTemplateConfig.java
+++ exRestTemplate/src/main/java/com/example/exresttemplate/config/RestTemplateConfig.java
@@ -12,6 +12,9 @@ import java.time.temporal.ChronoUnit;
 public class RestTemplateConfig {
 
     // RestTemplate 을 필요한 곳에서 주입받을 수 있도록
+    // RestTemplate을 만들 때 RestTemplateBuilder를 주입받는다.
+    // RestTemplateBuilder는 스프링부트를 사용하는 환경이면 바로 사용할 수 있다.
+    // 자동으로 타임아웃이 지정되지 않기 때문에 직접 해줘야 한다.
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder
