34ab694adf228aef85cf51624e55c94dcc7aa0c2
DodoNehir <rojalaurant@gmail.com>
2024-06-21 01:41:40 +0900

    delete 'first' directory

--- exRestTemplate/src/main/java/com/example/exresttemplate/entity/Member.java
+++ exRestTemplate/src/main/java/com/example/exresttemplate/entity/Member.java
@@ -1,2 +1,19 @@
-package com.example.exresttemplate.entity;public class Member {
+package com.example.exresttemplate.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class Member {
+    @Id
+    private int memberID;
+
+    @Column
+    private String teamId;
+
+    @Column
+    private String username;
 }
