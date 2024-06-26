a8f0f6125b3c49b11006fbc782ae3b51a9caee58
DodoNehir <rojalaurant@gmail.com>
2024-06-26 16:40:32 +0900

    delete batch project

---
+++ test-board/src/main/java/com/example/testboard/model/Post.java
@@ -0,0 +1,53 @@
+package com.example.testboard.model;
+
+import lombok.Getter;
+
+import java.time.ZonedDateTime;
+import java.util.Objects;
+
+@Getter
+public class Post {
+    private Long postId;
+    private String body;
+    private ZonedDateTime createdDateTime;
+
+    public Post(Long postId, String body, ZonedDateTime createdDateTime) {
+        this.postId = postId;
+        this.body = body;
+        this.createdDateTime = createdDateTime;
+    }
+
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void setCreatedDateTime(ZonedDateTime createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals(getPostId(), post.getPostId()) && Objects.equals(getBody(), post.getBody()) && Objects.equals(getCreatedDateTime(), post.getCreatedDateTime());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getPostId(), getBody(), getCreatedDateTime());
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "postId=" + postId +
+                ", body='" + body + '\'' +
+                ", createdDateTime=" + createdDateTime +
+                '}';
+    }
+}
