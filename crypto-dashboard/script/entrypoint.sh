5008b8f28b4c00da659277770e5f6b6d5c7ebb14
DodoNehir <rojalaurant@gmail.com>
2024-07-12 21:52:17 +0900

    [add] Docker compose.yaml and change comment

--- crypto-dashboard/script/entrypoint.sh
+++ crypto-dashboard/script/entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-# java -jar 사이에 java 옵션을 넣으면 된다.
-# 근데 한 두개가 아니라서 한꺼번에 통으로 받는 게 좋음
-# 2. command line 도 한 두개가 아니니 얘도 통으로 받음
-# entrypoint.sh을 실행할 때 전달된 커맨드 라인 명령 인자들을 대입해주는 @ 변수
+# entrypoint.sh을 실행할 때 전달된 커맨드 라인 명령 @
 exec java ${JAVA_OPTS} -jar /app.jar ${@}
