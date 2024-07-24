c7f48fa898445ed26ffab04fbeb6ec9e8756dde6
DodoNehir <rojalaurant@gmail.com>
2024-07-24 19:55:48 +0900

    [feat] simple employee controller

---
+++ project-voucher/src/main/java/com/example/projectvoucher/request/EmployeeCreateRequest.java
@@ -0,0 +1,2 @@
+package com.example.projectvoucher.request;public record EmployeeCreateRequest() {
+}
---
+++ project-voucher/src/main/java/com/example/projectvoucher/response/EmployeeResponse.java
@@ -0,0 +1,2 @@
+package com.example.projectvoucher.response;public record EmployeeResponse() {
+}
