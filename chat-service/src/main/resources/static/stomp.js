ba9439c9e4298d27c4a3e439127282c6b223ecbd
DodoNehir <rojalaurant@gmail.com>
2024-08-14 21:05:01 +0900

    [feat] edit html and js to use chatrooms

--- chat-service/src/main/resources/static/stomp.js
+++ chat-service/src/main/resources/static/stomp.js
@@ -5,14 +5,7 @@ const stompClient = new StompJs.Client({
 stompClient.onConnect = (frame) => {
   setConnected(true);
   console.log('Connected: ' + frame);
-  stompClient.subscribe('/sub/chats', (chatMessage) => {
-    showMessage(JSON.parse(chatMessage.body));
-  });
-  stompClient.publish({
-    destination: "/pub/chats",
-    body: JSON.stringify(
-        {'message': "connected"})
-  })
+
 };
 
 stompClient.onWebSocketError = (error) => {
@@ -27,12 +20,7 @@ stompClient.onStompError = (frame) => {
 function setConnected(connected) {
   $("#connect").prop("disabled", connected);
   $("#disconnect").prop("disabled", !connected);
-  if (connected) {
-    $("#conversation").show();
-  } else {
-    $("#conversation").hide();
-  }
-  $("#messages").html("");
+  $("#create").prop("disabled", connected);
 }
 
 function connect() {
@@ -45,9 +33,128 @@ function disconnect() {
   console.log("Disconnected");
 }
 
+function createChatroom() {
+  // 서버의 api 를 호출
+  $.ajax({
+    type: 'POST',
+    datatype: 'json',
+    url: 'chats?title=' + $('chatroom-title').val(),
+    success: function (data) {
+      console.log('data ', data);
+      showChatrooms();
+      enterChatrooms(data.id, true);
+    },
+    error: function (request, status, error) {
+      console.log('request: ' + request);
+      console.log('error: ' + error);
+    },
+  })
+}
+
+function showChatrooms() {
+  $.ajax({
+    type: 'GET',
+    datatype: 'json',
+    url: 'chats/',
+    success: function (data) {
+      console.log('data: ' + data);
+      renderChatrooms(data);
+    },
+    error: function (request, status, error) {
+      console.log('request: ' + request);
+      console.log("error: " + error);
+    },
+  })
+}
+
+function renderChatrooms(chatrooms) {
+  $("#chatroom-list").html("");
+  for (let i = 0; i < chatrooms.length; i++) {
+    $("#chatroom-list").append(
+        "<tr onclick='joinChatroom(" + chatrooms[i].id + ")'><td>"
+        + chatrooms[i].id + "</td><td>" + chatrooms[i].title + "</td><td>"
+        + chatrooms[i].memberCount + "</td><td>" + chatrooms[i].createdAt
+        + "</td></tr>"
+    )
+  }
+}
+
+// 같은 방에 있는 사람들끼리 대화를 주고받을 수 있게 해야한다.
+// stomp도 publish, subscribe를 구분해야 한다 (?)
+
+let subscription;
+
+// newMember가 처음 입장이면 입장 안내 메세지 출력
+function enterChatrooms(chatroomId, newMember) {
+  $("#chatroom-id").val(chatroomId);
+  $("#conversation").show();
+  $("#send").prop("disabled", false);
+  $("#leave").prop("disabled", false);
+
+  if (subscription != undefined) {
+    subscription.unsubscribe();
+  }
+
+  subscription = stompClient.subscribe('/sub/chats/' + chatroomId,
+      (chatMessage) => {
+        showMessage(JSON.parse(chatMessage.body));
+      });
+
+  if (newMember) {
+    stompClient.publish({
+      destination: "/pub/chats/" + chatroomId,
+      body: JSON.stringify(
+          {'message': " 님이 방에 들어왔습니다."})
+    })
+  }
+}
+
+function joinChatroom(chatroomId) {
+  $.ajax({
+    type: 'POST',
+    datatype: 'json',
+    url: '/chats/' + chatroomId,
+    success: function (data) {
+      console.log("data: " + data);
+      enterChatrooms(chatroomId, data);
+    },
+    error: function (request, status, error) {
+      console.log('request: ' + request);
+      console.log("error: " + error);
+    },
+  })
+}
+
+// 아예 나오는 것
+function leaveChatroom() {
+  let chatroomId = $("chatroom-id").val();
+  $.ajax({
+    type: "DELETE",
+    datatype: "json",
+    url: "/chats/" + chatroomId,
+    success: function (data) {
+      console.log("data: " + data);
+      showChatrooms();
+      exitChatroom(chatroomId);
+    },
+    error: function (request, status, error) {
+      console.log('request: ' + request);
+      console.log("error: " + error);
+    },
+  })
+}
+
+function exitChatroom(chatroomId) {
+  $("#chatroom-id").val("");
+  $("#conversation").hide();
+  $("#send").prop("disabled", true);
+  $("#leave").prop("disabled", true);
+}
+
 function sendMessage() {
+  let chatroomId = $("#chatroom-id").val();
   stompClient.publish({
-    destination: "/pub/chats",
+    destination: "/pub/chats" + chatroomId,
     body: JSON.stringify(
         {'message': $("#message").val()})
   });
@@ -64,5 +171,7 @@ $(function () {
   $("form").on('submit', (e) => e.preventDefault());
   $("#connect").click(() => connect());
   $("#disconnect").click(() => disconnect());
+  $("#create").click(() => createChatroom());
+  $("#leave").click(() => leaveChatroom());
   $("#send").click(() => sendMessage());
 });
